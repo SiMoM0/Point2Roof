0265074f56b495cb5b88c20c1c41d44b2ac32935
SiMoM0 <simomosco99@gmail.com>
2025-05-21 11:10:13 +0200

    update edge module

--- model/edge_pred_net.py
+++ model/edge_pred_net.py
@@ -9,7 +9,7 @@ from utils import loss_utils
 import pc_util
 import itertools
 import math
-from timm.models.layers import trunc_normal_
+from timm.models.layers import trunc_normal_, DropPath
 
 
 class EdgeAttentionNet(nn.Module):
@@ -18,8 +18,8 @@ class EdgeAttentionNet(nn.Module):
         self.model_cfg = model_cfg
         self.freeze = False
 
-        #self.att_layer = PairedPointAttention(input_channel)
-        self.att_layer = Attention(input_channel, num_heads=4)
+        # self.att_layer = PairedPointAttention(input_channel)
+        self.att_layer = EdgeModule(input_channel)
         num_feature = self.att_layer.num_output_feature
         self.shared_fc = LinearBN(num_feature, num_feature)
         self.drop = nn.Dropout(0.5)
@@ -221,7 +221,98 @@ class Attention(nn.Module):
 
         return x
 
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
 
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+class Block(nn.Module):
+
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, sr_ratio=1):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.norm2 = norm_layer(dim)
+        self.attn = Attention(
+            dim,
+            num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            attn_drop=attn_drop, proj_drop=drop, sr_ratio=sr_ratio)
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm3 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+    def forward(self, x1, x2):
+        x = x1 + self.drop_path(self.attn(self.norm1(x1), self.norm2(x2)))
+        x = x + self.drop_path(self.mlp(self.norm3(x)))
+
+        return x
+
+class EdgeModule(nn.Module):
+    def __init__(self, input_channel, block=True, drop_path=0.1):
+        super().__init__()
+        self.input_channel = input_channel
+        self.att1 = Block(input_channel, 4, drop_path=drop_path) if block else Attention(input_channel)
+        self.att2 = Block(input_channel, 4, drop_path=drop_path) if block else Attention(input_channel)
+        self.num_output_feature = input_channel
+        self.pool = nn.MaxPool1d(2)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, point_fea1, point_fea2):
+        fea1 = point_fea1 + self.drop_path(self.att1(point_fea1, point_fea2))
+        fea2 = point_fea2 + self.drop_path(self.att2(point_fea2, point_fea1))
+        # max pooling
+        fea = torch.cat([fea1.unsqueeze(1), fea2.unsqueeze(1)], 1)
+        fea = self.pool(fea.permute(0, 2, 1)).squeeze(-1)
+        return fea
 
 
 
